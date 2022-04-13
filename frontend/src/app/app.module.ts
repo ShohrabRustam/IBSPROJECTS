7321bb3e8b026d7a7fae9fce651ae9e7c6f5bf8c
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-13 22:33:38 +0530

    setup the componet

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,11 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import { HeaderComponent } from './components/header/header.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
 import { HomeComponent } from './components/home/home.component';
 import { DeskboardComponent } from './components/deskboard/deskboard.component';
-import {MatToolbarModule} from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
@@ -16,13 +16,15 @@ import {MatToolbarModule} from '@angular/material/toolbar';
     HeaderComponent,
     SidenavComponent,
     HomeComponent,
-    DeskboardComponent,
+    HeaderComponent,
+    DeskboardComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule
+    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
