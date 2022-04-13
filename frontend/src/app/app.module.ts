6eabee541461d329a07127d2f4cb7effb9272cbf
Shohrab Rustam <85831433+ShohrabRustam@users.noreply.github.com>
2022-04-13 22:13:36 +0530

    generate components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/header/header.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { HomeComponent } from './components/home/home.component';
+import { DeskboardComponent } from './components/deskboard/deskboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SidenavComponent,
+    HomeComponent,
+    DeskboardComponent
   ],
   imports: [
     BrowserModule,
